cde19f9f1bf7b8a6a4f0b5df028fd03395a6c51c
mike orgill <org21001@byui.edu>
2023-01-06 10:10:57 -0700

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What was your grade percentage? ");
+        string percentage = Console.ReadLine();
+        int grade = int.Parse(percentage);
+        string letter = "";
+
+        if(grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if(grade >= 80)
+        {
+            letter = "B";
+        }
+        else if(grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if(grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation you passed.");
+        }
+        else 
+        {
+            Console.WriteLine("You did not pass. Better luck next time.");
+        }
+
     }
 }
