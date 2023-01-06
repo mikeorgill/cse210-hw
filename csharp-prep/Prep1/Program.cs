4e6bfeb6a2ac5d97490e5044d37a50c04800368f
mike orgill <org21001@byui.edu>
2023-01-06 09:40:48 -0700

    Finished Prep 1 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Ask user for first name.
+        Console.Write("What is your first name? ");
+        string first_name = Console.ReadLine();
+
+        // Ask user for last name.
+        Console.Write("What is your last name? ");
+        string last_name = Console.ReadLine();
+
+        // Print the users name.
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+
     }
 }
