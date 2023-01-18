daff1d6ec0a9f4ee7f83ce1b59793f6331b53219
mike orgill <org21001@byui.edu>
2023-01-17 20:57:07 -0700

    Finished Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job Job1 = new Job();
+        Job1._jobTitle = "Software Engineer"; 
+        Job1._company = "Microsoft";
+        Job1._startYear = 2019;
+        Job1._endYear = 2022;
+
+        Job Job2 = new Job();
+        Job2._jobTitle = "Manager";
+        Job2._company = "Apple";
+        Job2._startYear = 2022;
+        Job2._endYear = 2023;
+
+        //Job1.Display();
+        //Job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+
+        myResume._jobs.Add(Job1);
+        myResume._jobs.Add(Job2);
+
+        myResume.Display();
+
+        //Console.WriteLine("Hello Learning02 World!");
     }
 }
