6a594405c0519b8d408462120f44fb08ed4f6551
mike orgill <org21001@byui.edu>
2023-02-28 21:41:51 -0700

    Finished learning lesson 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> sh = new List<Shape>();
+
+        Square sq = new Square("Blue", 4);
+        sh.Add(sq);
+
+        Recatangle re = new Recatangle("Yellow", 3, 4);
+        sh.Add(re);
+
+        Circle ci = new Circle("Purple", 4);
+        sh.Add(ci);
+
+        foreach (Shape s in sh)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            Console.WriteLine($"The {color} shape has an area of: {area}");
+
+
+        }
+        
+
+
     }
 }
